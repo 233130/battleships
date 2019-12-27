5b83132f419d44662ebff9a66bbe09e0d17547a3
233130 <58628286+233130@users.noreply.github.com>
2019-12-27 12:31:30 +0100

    Add files via upload
    Rozbudowa klas
    Dodanie umieszczanie statku na tablicy
    //TODO
    metoda field_hitted

--- battleships_beta.py
+++ battleships_beta.py
@@ -2,10 +2,16 @@ class Field:
     def __init__(self):
         self.symbol = "?"
         self.can_hit = False
-        self.can_
+        self.can_put_ship = False
 
     def print_symbol(self):
         print(self.symbol, "", end='')
+
+    def print_empty_symbol(self):
+        print(". ", end='')
+
+    def field_hitted(self):
+        pass
 
 
 class EmptyField(Field):
@@ -13,6 +19,7 @@ class EmptyField(Field):
         super().__init__()
         self.symbol = "."
         self.can_hit = True
+        self.can_put_ship = True
 
 
 class HitField(Field):
@@ -20,6 +27,10 @@ class HitField(Field):
         super().__init__()
         self.symbol = "X"
         self.can_hit = False
+        self.can_put_ship = False
+
+    def print_empty_symbol(self):
+        self.print_symbol()
 
 
 class MishitField(Field):
@@ -27,13 +38,20 @@ class MishitField(Field):
         super().__init__()
         self.symbol = "O"
         self.can_hit = False
+        self.can_put_ship = False
+
+    def print_empty_symbol(self):
+        self.print_symbol()
 
 
 class ShipField(Field):
-    def __init__(self, size):
+    def __init__(self, id, ship_id, size):
         super().__init__()
         self.symbol = size
         self.can_hit = True
+        self.can_put_ship = False
+        self.id = id
+        self.ship_id = ship_id
 
 
 class Ship:
@@ -43,51 +61,81 @@ class Ship:
         self.id = id
 
     def set_ship_on_board(self, board):
-        print("Dodajesz ", self.id, "statek o rozmiarze ", self.size)
+        print("Dodajesz statek o rozmiarze: ", self.size)
         problem = True
         while problem:
             x = int(input("Podaj wartość początkową wartość X: "))
             y = int(input("Podaj wartość początkową wartość Y: "))
             orientation = int(input("poziomo czy pionowo 0/1: "))
             problem = False
+            if x > (board.width - 1) or y > (board.high - 1):
+                print("Wyszedłeś po za tablicę")
+                problem = True
+                continue
             for i in range(self.size):
-                try:
-                    if orientation:
-                        field = board[y + i][x]
-                    else:
-                        field = board[y][x + i]
-                    print(field.__class__.__name__)
-                        # problem = True
-                        # print("Niestety nie mogę dodać tutaj statku")
-                        # break
-                except IndexError:
-                    problem = True
-                    print("Wyszedłeś po za tablicę")
-                    break
+                if orientation:
+                    field = board.get_obj(x, y + i)
+                else:
+                    field = board.get_obj(x + i, y)
 
+                if not field.can_put_ship:
+                    print("Pole zajęte")
+                    problem = True
+                    break
         for i in range(self.size):
+            mast = ShipField(i, self.id, self.size)
             if orientation:
-                board[y + i][x] = str(self.size)
+                board.change_field(x, y + i, mast)
             else:
-                board[y][x + i] = str(self.size)
+                board.change_field(x + i, y, mast)
+            self.fields.append(mast)
 
 
 class Board:
     def __init__(self, width, high):
         self.width = width
         self.high = high
-        self.board = []
-        for row in range(self.high):
-            self.board.append(EmptyField() for i in range(self.width))
+        self.board = [[EmptyField() for row in range(self.width)] for i in range(self.high)]
+        self.ships = []
+        self.board[1][2] = MishitField()
 
     def print_board(self):
         for row in self.board:
             for field in row:
                 field.print_symbol()
             print("")
+        print("")
+
+    def print_empty_board(self):
+        for row in self.board:
+            for field in row:
+                field.print_empty_symbol()
+            print("")
+
+    def get_obj(self, x, y):
+        return self.board[y][x]
+
+    def change_field(self, x, y, field):
+        self.board[y][x] = field
+
+    def board_hit(self, x, y):
+        field = self.get_obj(x, y)
+        if field.can_hit:
+            if field.can_put_ship:
+                self.change_field(x, y, MishitField())
+            else:
+                pass
+        else:
+            print("Nie możesz uderzyć w dane pole")
 
 
 tablica = Board(8, 6)
 tablica.print_board()
-statek = Ship(1,5)
-statek.set_ship_on_board(tablica)
+for i in range(2):
+    rozmiar = int(input("Podaj wielkość statku: "))
+    statek = Ship(i, rozmiar)
+    statek.set_ship_on_board(tablica)
+    tablica.ships.append(statek)
+    tablica.print_board()
+tablica.board_hit(1, 1)
+tablica.print_board()
